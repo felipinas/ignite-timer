d941ebe3cdb0b04d2c1b4822930de8d3c5309725
felipinas <jfss2@cin.ufpe.br>
2022-08-20 18:23:09 -0300

    refact: change import order in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
+import { NavLink } from 'react-router-dom';
 import { Timer, Scroll } from 'phosphor-react';
 
 import { HeaderContainer } from './styles';
 import logoIgnite from '../../assets/logo-ignite.svg';
-import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
