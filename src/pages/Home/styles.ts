bf7a10ecf7393d4c2e38e2cc5430980ad024abdf
felipinas <jfss2@cin.ufpe.br>
2022-08-21 00:50:06 -0300

    feat: stylize the home page

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,125 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.main`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`;
+
+export const FormContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+`;
+
+const BaseInput = styled.input`
+  height: 2.5rem;
+
+  padding: 0 0.5rem;
+
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  background: transparent;
+
+  font-weight: bold;
+  font-size: 1.125rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
+
+export const CountdownContainer = styled.div`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+
+  color: ${(props) => props.theme['gray-100']};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: ${(props) => props.theme['gray-700']};
+
+    padding: 2rem 1rem;
+
+    border-radius: 8px;
+  }
+`;
+
+export const Separator = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 4rem;
+  padding: 2rem 0;
+
+  color: ${(props) => props.theme['green-500']};
+  overflow: hidden;
+`;
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  padding: 1.25rem 0;
+
+  color: ${(props) => props.theme['gray-100']};
+  background: ${(props) => props.theme['green-500']};
+  font-weight: bold;
+
+  border: 0;
+  border-radius: 8px;
+
+  cursor: pointer;
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`;
