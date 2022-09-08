79a81595a62dec84f2a9ec9ce2f7079e1a2d7e9f
felipinas <jfss2@cin.ufpe.br>
2022-09-07 23:46:41 -0300

    fix: add return in generator function in use reducer

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -54,6 +54,11 @@ export function CyclesContextProvider({
       if (storedStateAsJSON) {
         return JSON.parse(storedStateAsJSON);
       }
+
+      return {
+        cycles: [],
+        activeCycleId: null,
+      };
     },
   );
 
